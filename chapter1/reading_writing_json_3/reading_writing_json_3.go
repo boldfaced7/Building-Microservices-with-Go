65162692afa9f91ba7441e404aad1cc78b067537
준모 강 <boldfaced7@hanyang.ac.kr>
2022-05-17 17:47:56 +0900

    I

---
+++ chapter1/reading_writing_json_3/reading_writing_json_3.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	port := 8080
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
+
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "Hello, World!"}
+	encoder := json.NewEncoder(w) // 열려 있는 Writer에 JSON을 바로 쓸 수 있는 Encoder 객체를 반환
+	encoder.Encode(response)
+}
